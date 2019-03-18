3efeef7bb7cc698924ae46b84b00247f9345187f
linzhen <273487984@qq.com>
2019-03-18 22:56:25 +0800

    auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -21,7 +22,15 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        // xss过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如果 slug 没内容，使用即时翻译对 title 进行翻译
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
